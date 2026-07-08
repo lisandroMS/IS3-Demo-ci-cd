179d14d5edc92d259408b83dd39c63e96f1fad05
lisa_000800 <schoembergerlisandro@gmail.com>
2026-07-07 22:30:31 -0300

    agregar variable de entorno

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+const API_URL = import.meta.env.VITE_API_URL || "https://is3-demo-ci-cd-0ib1.onrender.com/";
 
 const formatARS = (value) =>
   new Intl.NumberFormat("es-AR", {
